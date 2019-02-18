6d58ce31777d612ff224f0cbff086dbd77c7a6c6
Vinicios Clarindo <vinicioslc@outlook.com>
2019-02-18 00:30:04 -0300

    Updated Example.

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -59,7 +59,11 @@ class _MyHomePageState extends State<MyHomePage> {
     //update your domain before using
     /*socketIO = new SocketIO("http://127.0.0.1:3000", "/chat",
         query: "userId=21031", socketStatusCallback: _socketStatus);*/
-    socketIO = SocketIOManager().createSocketIO("http://127.0.0.1:3000", "/chat", query: "userId=21031", socketStatusCallback: _socketStatus);
+    socketIO = SocketIOManager().createSocketIO(
+        domain: "http://127.0.0.1:3000",
+        namespace: "/chat",
+        query: "userId=21031",
+        socketStatusCallback: _socketStatus);
 
     //call init socket before doing anything
     socketIO.init();
@@ -72,7 +76,11 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   _connectSocket02() {
-    socketIO02 = SocketIOManager().createSocketIO("http://127.0.0.1:3000", "/map", query: "userId=21031", socketStatusCallback: _socketStatus02);
+    socketIO02 = SocketIOManager().createSocketIO(
+        domain: "http://127.0.0.1:3000",
+        namespace: "/map",
+        query: "userId=21031",
+        socketStatusCallback: _socketStatus02);
 
     //call init socket before doing anything
     socketIO02.init();
@@ -197,8 +205,8 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             new RaisedButton(
-              child:
-                  const Text('CONNECT  SOCKET 01', style: TextStyle(color: Colors.white)),
+              child: const Text('CONNECT  SOCKET 01',
+                  style: TextStyle(color: Colors.white)),
               color: Theme.of(context).accentColor,
               elevation: 0.0,
               splashColor: Colors.blueGrey,
@@ -208,8 +216,8 @@ class _MyHomePageState extends State<MyHomePage> {
               },
             ),
             new RaisedButton(
-              child:
-              const Text('CONNECT SOCKET 02', style: TextStyle(color: Colors.white)),
+              child: const Text('CONNECT SOCKET 02',
+                  style: TextStyle(color: Colors.white)),
               color: Theme.of(context).accentColor,
               elevation: 0.0,
               splashColor: Colors.blueGrey,
@@ -219,7 +227,8 @@ class _MyHomePageState extends State<MyHomePage> {
               },
             ),
             new RaisedButton(
-              child: const Text('SEND MESSAGE', style: TextStyle(color: Colors.white)),
+              child: const Text('SEND MESSAGE',
+                  style: TextStyle(color: Colors.white)),
               color: Theme.of(context).accentColor,
               elevation: 0.0,
               splashColor: Colors.blueGrey,
